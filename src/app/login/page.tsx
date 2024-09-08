7bf4c333938ab3ede33bd625a7221a74392d3720
kashim rahman mahdi <mahdi.rahman356@gmail.com>
2024-09-08 19:21:43 +0600

    set up project

---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+
+const page = () => {
+    return (
+        <div className="bg-white text-slate-800">
+            login
+        </div>
+    );
+};
+
+export default page;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,23 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: [
+      {
+        craftyBayTheme: {
+          "primary": "#a991f7",
+          "secondary": "#f6d860",
+          "accent": "#37cdbe",
+          "neutral": "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
 };
 export default config;
